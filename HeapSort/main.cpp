5104a7b56058c9dd6ccbb6e434ce8e1dce458d69
Stanley Tsang <stsang@Stanleys-MacBook-Air.local>
2016-08-03 00:02:56 -0400

    First commit

---
+++ HeapSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HeapSort
+//
+//  Created by Stanley Tsang on 2016-08-02.
+//  Copyright (c) 2016 Stanley Tsang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) 	{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
